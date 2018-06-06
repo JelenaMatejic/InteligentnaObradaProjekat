a1068dfe730fa5ddf8870ebd6eed2edeadacee76
JelenaMatejic <jelena.jelena.matejic@gmail.com>
2018-06-06 08:34:41 +0200

    ReadingFromFile class + Window class

---
+++ ReadingFromFile.py
@@ -0,0 +1,10 @@
+def readDataFromFile(fileName, splitChar):
+    data = []
+    textFile = open(fileName, "r")
+    # Svaka od linija u .txt fajlu treba biti jedan pimer tj. jedan vektor u data set-u
+    for line in textFile:
+        line = line.strip('\n')
+        sample = line.split(splitChar)
+        sample = [float(i) for i in sample]
+        data.append(sample)
+    return data
