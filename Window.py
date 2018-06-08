9fa30163be79d34ee5c7614ebaf58561e32547ca
JelenaMatejic <jelena.jelena.matejic@gmail.com>
2018-06-08 09:38:38 +0200

    Looks of main window

--- Window.py
+++ Window.py
@@ -7,6 +7,7 @@ import matplotlib
 matplotlib.use('TkAgg')
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from tkinter import *
+from tkinter import filedialog
 from matplotlib.figure import Figure
 
 # root.title("Multiclass One VS All")
@@ -19,15 +20,26 @@ class windowClass:
         self.canvas = None
         self.window = window
         self.box = Entry(window)
-        self.buttonPerceptron = Button (window, text="Perceptron", command=self.plotPerceptron)
-        self.buttonLogisticRegression = Button (window, text="Logistic Regression", command=self.plotLogisticRegression)
-        self.buttonMulticlassOneVsAllPerceptron = Button(window, text="One Vs All - Perceptron", command=self.plotMulticlassOneVsAllPerceptron)
-        self.buttonMulticlassOneVsAllLogisticRegression = Button(window, text="One Vs All - Logistic Regression",
-                                                                 command=self.plotMulticlassOneVsAllLogisticRegression)
+        # self.filename = filedialog.askopenfilename(initialdir="/", title="Select file",
+        #                                              filetypes=(("jpeg files", "*.jpg"), ("all files", "*.*")))
+        self.labelForFile = Label(window, text = "No file chosen . . .", bg = "white", height=1, width=50,  anchor="e")
+        self.buttonChooseFile = Button(window, text="Choose Fle")
+        self.buttonPerceptron = Button (window, text="Perceptron Algorithm", command=self.plotPerceptron, height = 1, width = 50)
+        self.buttonLogisticRegression = Button (window, text="Logistic Regression Algorithm", command=self.plotLogisticRegression, height = 1, width = 50)
+        self.buttonPassiveAggressive = Button (window, text="Passive Aggressive Algorithm", command=self.plotLogisticRegression, height = 1, width = 50)
+        self.buttonMulticlassOneVsAllPerceptron = Button(window, text="One Vs All - Perceptron Algorithm", command=self.plotMulticlassOneVsAllPerceptron, height = 1, width = 50)
+        self.buttonMulticlassOneVsAllLogisticRegression = Button(window, text="One Vs All - Logistic Regression algorithm",
+                                                                 command=self.plotMulticlassOneVsAllLogisticRegression, height = 1, width = 50)
+        self.buttonPassiveAggressiveOneVsAllLogisticRegression = Button(window, text="One Vs All - Logistic Regression algorithm",
+                                                                 command=self.plotMulticlassOneVsAllLogisticRegression, height=1, width=50)
+        self.labelForFile.grid(row=0, column=0)
+        self.buttonChooseFile.grid(row=0, column=1, sticky=W)
         self.buttonPerceptron.grid(row=1, column=0)
-        self.buttonMulticlassOneVsAllPerceptron.grid(row=1, column=1)
         self.buttonLogisticRegression.grid(row=2, column=0)
+        self.buttonPassiveAggressive.grid(row=3, column=0)
+        self.buttonMulticlassOneVsAllPerceptron.grid(row=1, column=1)
         self.buttonMulticlassOneVsAllLogisticRegression.grid(row=2, column=1)
+        self.buttonPassiveAggressiveOneVsAllLogisticRegression.grid(row=3, column=1)
 
     def plotPerceptron (self):
         fig = Perceptron.perceptronPlotInWindow()
@@ -36,12 +48,12 @@ class windowClass:
     def drawFigure(self, fig):
         if self.canvas is None:
             self.canvas = FigureCanvasTkAgg(fig, master=self.window)
-            self.canvas.get_tk_widget().grid(row = 3, columnspan=2)
+            self.canvas.get_tk_widget().grid(row = 4, columnspan=2)
             self.canvas.draw()
         else:
             self.canvas.get_tk_widget().destroy()
             self.canvas = FigureCanvasTkAgg(fig, master=self.window)
-            self.canvas.get_tk_widget().grid(row = 3, columnspan=2)
+            self.canvas.get_tk_widget().grid(row = 4, columnspan=2)
             self.canvas.draw()
 
     def plotLogisticRegression(self):
@@ -62,8 +74,10 @@ class windowClass:
 
 window= Tk()
 windowClass (window)
-window.geometry("650x600")
+window.geometry("720x600")
 window.configure(background='white')
 window.title("Classifiers")
+#filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+#print(filename)
 window.mainloop()
 window.quit()
