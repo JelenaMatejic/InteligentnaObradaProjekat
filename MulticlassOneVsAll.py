8518cc536c53b49f307d92fc32d4f8109683b998
JelenaMatejic <jelena.jelena.matejic@gmail.com>
2018-06-08 12:08:16 +0200

    Perceptron without cross validation

--- MulticlassOneVsAll.py
+++ MulticlassOneVsAll.py
@@ -59,6 +59,17 @@ def oneVsAllLogisticRegressionExample():
     fig = Plot.plotLinesMulticlassOneVsAll(x, t, w, b)
     return fig
 
+    data = ReadingFromFile.readDataFromFile(file, ',')  # Podaci ucitani iz fajla
+    trainingSet, testSet = CrossValidation.makeSets(data)  # Napravimo trening i test set
+    kTrainingSets, kValidSets = CrossValidation.kCrossValidationMakeSets(trainingSet, 5)  # Napravimo k trening i test set-ova unakrsnom validacijom (k = 5)
+
+    w, b = crossTrain(kTrainingSets, kValidSets)  # Istreniramo k trening setova i kao rezultat vratimo najbolje w i najbolje b  (ono w i b za koje je greska bila najmanja)
+    x, t = Initializing.processData(testSet)  # Rezultat crtamo i merimo nad test skupom podataka
+
+    fig = Plot.plotInWindow(x, w, b, t)
+    return fig
+
+
 #oneVsAllLogisticRegressionExample()
 #oneVsAllPerceptronExample()
 
