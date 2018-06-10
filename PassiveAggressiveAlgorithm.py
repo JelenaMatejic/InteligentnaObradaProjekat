7751578d35559e7c78856acacc2a58613a69355a
JelenaMatejic <jelena.jelena.matejic@gmail.com>
2018-06-10 13:19:59 +0200

    Passive Aggressive Algorithm set param c

--- PassiveAggressiveAlgorithm.py
+++ PassiveAggressiveAlgorithm.py
@@ -18,41 +18,38 @@ def lambd(ln, xn, c):
     l = min(c, tmp)
     return l
 
-def train(x, t, w, b, c = 0.01, epohe = 100):
+def train(x, t, w, b, c, epohe = 100):
     for e in range(epohe):
         for n in range(len(x)):
-            tn = np.array(t[0,n])
-            xn = np.array(x[n])
+            tn = np.array(t[0,n]).reshape((1, 1))
+            xn = np.array(x[n]).reshape((1,len(x[n])))
             ln = loss(tn, w, xn)
             lamN = lambd(ln, xn, c)
 
             # pomerimo w i b
             if ln > 0:
-                w = w + np.dot(lamN*tn, xn)
+                w = w + np.dot(np.dot(lamN,tn), xn)
                 b = b + np.dot(lamN, tn)
 
     return [w, b]
 
-def sumLoss(trainingSet, w, b, c):
+def sumLoss(kValidSets, w):
     sum = 0
-    #t = labels(trainingSet)
-    for n in range(len(trainingSet)):
-        tn = np.array(t[n]).reshape((1, 1))
-        xn = np.array(trainingSet[n][:-1]).reshape((1, len(trainingSet[n]) - 1))
+    xValid, tValid = Initializing.processData(kValidSets[0])
+    for n in range(len(xValid)):
+        tn = np.array(tValid[0,n]).reshape((1, 1))
+        xn = np.array(xValid[n]).reshape((1, len(xValid[n])))
         ln = loss(tn, w, xn)
         sum += ln
-
     return sum
 
-def optC(trainingSet, validSet, w, b, cSet = [0.01, 0.1, 10, 100, 1000]):
-    minLoss = math.inf
+def optC(kTrainingSets, kValidSets, cSet = [0.01, 0.1, 10, 100, 1000]):
+    minLoss = math.inf # posto su gubici negativni, najmanji je onajkoji je najblizi nuli
     optimalC = cSet[0]
     for c in cSet:
-        w = np.zeros((1, len(data[0]) - 1))
-        b = np.array([[0]])
-        w, b = train(trainingSet, w, b, c)
-        sumL = sumLoss(validSet, w, b, c)
-        if minLoss > sumL:
+        w, b = crossTrain(kTrainingSets, kValidSets, c)
+        sumL = sumLoss(kValidSets, w)
+        if minLoss < abs(sumL[0][0]):
             minLoss = sumL
             optimalC = c
     return [optimalC]
@@ -69,13 +66,13 @@ def testResults(testSet, w, b, c):
             tResult.append(-1)
     return tResult
 
-def crossTrain(kTrainingSets, kValidSets):
+def crossTrain(kTrainingSets, kValidSets, c):
 
     for k in range(len(kTrainingSets)):
         # Trenirano parametre na k-tom trening set-u
         x, t = Initializing.processData(kTrainingSets[k])
         w, b = Initializing.initialParam(x)
-        w, b = train(x, t, w, b)
+        w, b = train(x, t, w, b, c)
 
         # # Merino gresku na k-tom validacionom set-u, za dobijene parametre w i t
         # xValid, tValid = Initializing.processData(kValidSets[k])
@@ -88,12 +85,16 @@ def crossTrain(kTrainingSets, kValidSets):
     # return [bestW, bestB]
     return [w, b]
 
-data = ReadingFromFile.readDataFromFile('./dataSets/PerceptronDataSet.txt', ',')
-trainingSet, testSet = CrossValidation.makeSets(data)  # Napravimo trening i test set
-kTrainingSets, kValidSets = CrossValidation.kCrossValidationMakeSets(trainingSet,5)  # Napravimo k trening i test set-ova unakrsnom validacijom (k = 5)
-w, b = crossTrain(kTrainingSets, kValidSets)  # Istreniramo k trening setova i kao rezultat vratimo najbolje w i najbolje b  (ono w i b za koje je greska bila najmanja)
+def PassiveAggressiveAlgorithmExample():
+    data = ReadingFromFile.readDataFromFile('./dataSets/PerceptronDataSet.txt', ',')
+    trainingSet, testSet = CrossValidation.makeSets(data)  # Napravimo trening i test set
+    kTrainingSets, kValidSets = CrossValidation.kCrossValidationMakeSets(trainingSet,5)  # Napravimo k trening i test set-ova unakrsnom validacijom (k = 5)
+    c = optC(kTrainingSets, kValidSets)
+    w, b = crossTrain(kTrainingSets, kValidSets, c)  # Istreniramo k trening setova i kao rezultat vratimo najbolje w i najbolje b  (ono w i b za koje je greska bila najmanja)
 
-x, t = Initializing.processData(testSet)  # Rezultat crtamo i merimo nad test skupom podataka
-Plot.plotData(x, t)
-Plot.plotLine(x, w, b)
-plt.show()
+    x, t = Initializing.processData(testSet)  # Rezultat crtamo i merimo nad test skupom podataka
+    Plot.plotData(x, t)
+    Plot.plotLine(x, w, b)
+    plt.show()
+
+
